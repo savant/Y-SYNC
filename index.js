dd099d20a5842c15108fa0908b8b3c47f2cae11f
savant <babatundeakinsonu@gmail.com>
2022-03-11 20:19:14 -0600

    Changed to vercel

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
 //express
-const express = require('express'); //importing express module
+const express = require("express"); //importing express module
 const app = express(); //creating express app
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const {
-  Server
-} = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server);
-
-
 const port = process.env.PORT || 3000; //set express port
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/landing.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/landing.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('event', (msg, room) => {
-    io.in(room).emit('event', msg);
-  })
+io.on("connection", (socket) => {
+  socket.on("event", (msg, room) => {
+    io.in(room).emit("event", msg);
+  });
 
-  socket.on('send-message', (msg, room, userName) => {
-    io.in(room).emit('receive-msg', msg, userName);
-  })
+  socket.on("send-message", (msg, room, userName) => {
+    io.in(room).emit("receive-msg", msg, userName);
+  });
 
   function joinRoom(socket, room) {
     if (socket.room) {
       console.log("leaving room: " + socket.room);
+      io.in(room).emit("receive-msg", msg, userName);
       socket.leave(socket.room);
     }
     socket.join(room);
     socket.room = room;
-    console.info(socket.id + ' joined room ' + room, socket.room);
+    console.info(socket.id + " joined room " + room, socket.room);
   }
 
-  socket.on('join-room', (room, cb) => {
+  socket.on("join-room", (room, cb) => {
     joinRoom(socket, room);
     cb(`You joined room: ${room}`);
-  })
-  socket.on('typing', (msg, room) => {
-    io.in(room).emit('typing', msg);
-  })
+  });
+  socket.on("typing", (msg, room) => {
+    io.in(room).emit("typing", msg);
+  });
 });
 
 server.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
-
-
 
 //https://gist.github.com/kylewelsby/2b49d2db31d45b939479
--- public/chat.js
+++ public/chat.js
@@ -4,75 +4,77 @@ const userHandle = document.getElementById("handle");
 
 const roomInput = document.getElementById("room-input");
 const messageInput = document.getElementById("message");
-const typingFeedback = document.getElementById('feedback');
-const chatOutput = document.getElementById('output');
+const typingFeedback = document.getElementById("feedback");
+const chatOutput = document.getElementById("output");
 
-
-socket.on('connect', () => {
-    const randRoom = Math.random().toString(36).substring(7);
-    displayMessage("connect", socket);
-    const room = roomInput.defaultValue = randRoom;
-    socket.emit('join-room', room, message => {
-        // displayMessage("join", room);
-    });
-})
-
-socket.on('receive-msg', (message, userName) => {
-    const data = {
-        userHandle: userName,
-        msg: message
-    };
-    displayMessage("msg", data);
-})
-
-socket.on('typing', (userName) => {
-    displayMessage("typing", userName)
+socket.on("connect", () => {
+  const randRoom = Math.random().toString(36).substring(7);
+  displayMessage("connect", socket);
+  const room = (roomInput.defaultValue = randRoom);
+  socket.emit("join-room", room, (message) => {
+    // displayMessage("join", room);
+  });
 });
 
+socket.on("receive-msg", (message, userName) => {
+  const data = {
+    userHandle: userName,
+    msg: message,
+  };
+  displayMessage("msg", data);
+});
 
-sendButton.addEventListener("click", e => {
-    e.preventDefault();
-    sendMessage();
+socket.on("typing", (userName) => {
+  displayMessage("typing", userName);
+});
+
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendMessage();
 });
 
 function sendMessage() {
-    const message = messageInput.value;
-    const room = roomInput.value;
-    const userName = userHandle.value;
+  const message = messageInput.value;
+  const room = roomInput.value;
+  const userName = userHandle.value;
 
-    if (message === "") return;
+  if (message === "") return;
 
-    socket.emit('send-message', message, room, userName);
-    document.getElementById('chat-window').scrollTop = document.getElementById('chat-window').scrollHeight;
+  socket.emit("send-message", message, room, userName);
+  document.getElementById("chat-window").scrollTop =
+    document.getElementById("chat-window").scrollHeight;
 
-    messageInput.value = "";
+  messageInput.value = "";
 }
 
 function JoinRoom() {
-    const room = roomInput.value;
-    socket.emit('join-room', room, message => {
-        displayMessage("join", room);
-    });
+  const room = roomInput.value;
+  socket.emit("join-room", room, (message) => {
+    displayMessage("join", room);
+  });
 }
 
-messageInput.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        sendMessage();
-    } else {
-        const room = roomInput.value;
-        socket.emit('typing', userHandle.value, room);
-    }
-})
+messageInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  } else {
+    const room = roomInput.value;
+    socket.emit("typing", userHandle.value, room);
+  }
+});
 
 function displayMessage(action, data) {
-    typingFeedback.innerHTML = '';
-    if (action === "connect") {
-        chatOutput.innerHTML += '<p><strong> You connected with id: ' + data.id;
-    } else if (action === "msg") {
-        chatOutput.innerHTML += '<p><strong>' + data.userHandle + ': </strong>' + data.msg + '</p>';
-    } else if (action === "typing") {
-        typingFeedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
-    } else if (action === "join") {
-        chatOutput.innerHTML += '<p><strong>' + 'You joined room:  </strong>' + data + '</p>';
-    }
+  typingFeedback.innerHTML = "";
+  if (action === "connect") {
+    chatOutput.innerHTML += "<p><strong> You connected with id: " + data.id;
+  } else if (action === "msg") {
+    chatOutput.innerHTML +=
+      "<p><strong>" + data.userHandle + ": </strong>" + data.msg + "</p>";
+  } else if (action === "typing") {
+    typingFeedback.innerHTML =
+      "<p><em>" + data + " is typing a message...</em></p>";
+  } else if (action === "join") {
+    chatOutput.innerHTML +=
+      "<p><strong>" + "You joined room:  </strong>" + data + "</p>";
+  }
 }
